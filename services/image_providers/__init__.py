2fe2cfccf71e48775e5f2cb139b47322ffb31297
FARINAZZO Cédric <cedric.farinazzo@epita.fr>
2025-04-13 18:32:40 +0200

    image_providers: add providers lists

--- services/image_providers/__init__.py
+++ services/image_providers/__init__.py
@@ -1,2 +1,8 @@
+from .base import BaseImageProvider
 from .debian import DebianImageProvider
 from .ubuntu import UbuntuImageProvider
+
+IMAGE_PROVIDERS : dict[str, BaseImageProvider] = {
+    "debian": DebianImageProvider,
+    "ubuntu": UbuntuImageProvider,
+}
